eeb68f118f3a034754dc61e409f1ade4f6ce28ff
Michael <hnthu.aung@gmail.com>
2024-11-24 17:26:41 +0630

    card

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,11 +1,23 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="sm:flex flex-wrap max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5">
+
+        <!-- label -->
+        <div class="w-full sm:hidden font-semibold text-xl text-center text-gray-900 dark:text-gray-100">Products</div>
+
+        <!-- card -->
+        @foreach ($products as $product)
+            <div
+                class="text-gray-900 bg-white dark:text-gray-100 dark:bg-gray-800 w-64 rounded-lg mt-4 sm:ml-8 mx-auto sm:mx-0
+            transition-all ease-in-out duration-300 hover:-translate-y-1 hover:scale-105">
+                <div class="p-1">
+                    <img src="https://images.pexels.com/photos/1306548/pexels-photo-1306548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt="something" class="w-full h-56 rounded-md">
+                    <header class="text-lg text-center font-medium">{{ $product->name }}</header>
+                    <p class="text-center">{{ $product->price }}</p>
+                    <button class=" bg-blue-700 hover:bg-blue-600 rounded-lg w-full mt-1">Detail</button>
+                    <button class="bg-green-600 hover:bg-green-500 rounded-lg w-full mt-3 sm:mt-1">Add to Cart</button>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
@@ -12,13 +13,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/products', function () {
-    $products = new Product();
-    $products->name = 'Para';
-    $products->price = 120;
-    $products->id = 1;
-    return view('products',['products' => $products]);
-})->middleware(['auth', 'verified'])->name('products');
+Route::get('/products', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('products');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
